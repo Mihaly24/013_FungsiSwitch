174e4f5ad8082c72d3e1fbdf491bb15dd07b3252
Mihaly24 <fairuzk20@gmail.com>
2024-03-26 17:35:03 +0700

    Menambahkan prosedur penjumlahan

---
+++ Satu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+    cout << "Masukkan bilangan pertama = ";
+    cin >> bilA;
+    cout << "Masukkan bilangan kedua = ";
+    cin >> bilB;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
